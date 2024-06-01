08032ae3ed5dcacc262dc82ae17916ce150fdf6d
pronoy656 <pronoypaul56@gmail.com>
2024-06-01 16:58:13 +0600

    create axios hook and create biodata

--- src/Component/UserDashBoard/EditBioData.jsx
+++ src/Component/UserDashBoard/EditBioData.jsx
@@ -2,6 +2,246 @@ const EditBioData = () => {
   return (
     <div>
       <h1>This is bio data page</h1>
+      <div className="flex space-x-4">
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text"> Biodata Type</span>
+          </label>
+          <select className="select select-bordered">
+            <option disabled selected>
+              Pick one Biodata type
+            </option>
+            <option value={"Male"}>Male</option>
+            <option value={"Female"}>Female</option>
+          </select>
+        </div>
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+      </div>
+      <div className="flex space-x-4">
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text"> Job Category</span>
+          </label>
+          <select className="select select-bordered">
+            <option disabled selected>
+              Pick one Job category
+            </option>
+            <option value={"On-site Job"}>On-site Job</option>
+            <option value={"Remote Job"}>Remote Job</option>
+            <option value={"Hybrid"}>Hybrid</option>
+            <option value={"Part-Time"}>Part-Time</option>
+          </select>
+        </div>
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+      </div>
+      <div className="flex space-x-4">
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+      </div>
+      <div className="flex space-x-4">
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+      </div>
+      <div className="flex space-x-4">
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+      </div>
+      <div className="flex space-x-4">
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text"> Job Category</span>
+          </label>
+          <select className="select select-bordered" name="category">
+            <option disabled selected>
+              Pick one Job category
+            </option>
+            <option value={"On-site Job"}>On-site Job</option>
+            <option value={"Remote Job"}>Remote Job</option>
+            <option value={"Hybrid"}>Hybrid</option>
+            <option value={"Part-Time"}>Part-Time</option>
+          </select>
+        </div>
+      </div>
+      <div className="flex space-x-4">
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text"> Job Category</span>
+          </label>
+          <select className="select select-bordered" name="category">
+            <option disabled selected>
+              Pick one Job category
+            </option>
+            <option value={"On-site Job"}>On-site Job</option>
+            <option value={"Remote Job"}>Remote Job</option>
+            <option value={"Hybrid"}>Hybrid</option>
+            <option value={"Part-Time"}>Part-Time</option>
+          </select>
+        </div>
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+      </div>
+      <div className="flex space-x-4">
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+      </div>
+      <div className="flex space-x-4">
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+        <div className="form-control w-1/2">
+          <label className="label">
+            <span className="label-text">Name</span>
+          </label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter Your name"
+            className="input input-bordered"
+            required
+          />
+        </div>
+      </div>
     </div>
   );
 };
