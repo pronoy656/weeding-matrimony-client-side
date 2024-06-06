3d6d1614e0327259d8bb910aa20005ffe39a3a71
pronoy656 <pronoypaul56@gmail.com>
2024-06-07 00:22:17 +0600

    update premium member and create premium member in one method

--- src/Component/AdminDashBoard/AprvPremiumTable.jsx
+++ src/Component/AdminDashBoard/AprvPremiumTable.jsx
@@ -1,24 +1,46 @@
 import Swal from "sweetalert2";
 import useAxiosSecure from "../Hooks/useAxiosSecure";
+import useUser from "../Hooks/useUser";
 
 const AprvPremiumTable = ({ premium, index }) => {
-  const { email, Name, _id } = premium;
-  //   const axiosSecure = useAxiosSecure();
+  const {
+    email,
+    Name,
+    _id,
+    Biodata_Type,
+    Profile_Image_Link,
+    Permanent_Division,
+    Age,
+    Occupation,
+  } = premium;
+  const axiosSecure = useAxiosSecure();
+  const [, refetch] = useUser();
+  const premiumData = {
+    _id,
+    email: email,
+    Name,
+    Biodata_Type,
+    Profile_Image_Link,
+    Permanent_Division,
+    Age,
+    Occupation,
+  };
 
-  //   const handleMakePremium = () => {
-  //     axiosSecure.patch(`/users/premium/${_id}`).then((res) => {
-  //       console.log(res.data);
-  //       if (res.data.modifiedCount > 0) {
-  //         Swal.fire({
-  //           position: "top-end",
-  //           icon: "success",
-  //           title: `${Name}is now premium member`,
-  //           showConfirmButton: false,
-  //           timer: 1500,
-  //         });
-  //       }
-  //     });
-  //   };
+  const handleMakePremium = () => {
+    axiosSecure.post(`/users/premiumCltBio/${_id}`, premiumData).then((res) => {
+      console.log(res.data);
+      if (res.data.modifiedCount) {
+        refetch();
+        Swal.fire({
+          position: "top-end",
+          icon: "success",
+          title: `${Name}is now premium member`,
+          showConfirmButton: false,
+          timer: 1500,
+        });
+      }
+    });
+  };
 
   return (
     <div>
@@ -27,7 +49,20 @@ const AprvPremiumTable = ({ premium, index }) => {
         <td>{email}</td>
         <td>{Name}</td>
         <td>
-          <button className="btn btn-md bg-yellow-400">Make Premium</button>
+          {premium.role === "premium" ? (
+            <>
+              <h1 className="btn btn-md bg-purple-300">Premium member</h1>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={handleMakePremium}
+                className="btn btn-md bg-yellow-400"
+              >
+                Make Premium
+              </button>
+            </>
+          )}
         </td>
       </tr>
     </div>
