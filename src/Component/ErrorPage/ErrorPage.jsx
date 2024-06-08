9998516facc6b9298a6208dc10dfe6f41183b3d8
pronoy656 <pronoypaul56@gmail.com>
2024-06-08 17:41:29 +0600

    lottie react add on error page

--- src/Component/ErrorPage/ErrorPage.jsx
+++ src/Component/ErrorPage/ErrorPage.jsx
@@ -1,7 +1,22 @@
+import Lottie from "lottie-react";
+import errorPage from "../../../src/errorPage (2).json";
+
 const ErrorPage = () => {
   return (
     <div>
-      <h1>This is error page</h1>
+      <h1 className="text-3xl font-semibold text-red-500 text-center mt-7">
+        OOOOPPPPPSSSS!!!!!!!!!
+      </h1>
+      <div className="flex justify-center">
+        <Lottie
+          className="w-[400px] h-[400px]"
+          animationData={errorPage}
+        ></Lottie>
+      </div>
+      <div className="flex justify-center items-center">
+        <h1 className="text-3xl font-semibold">Back to the</h1>{" "}
+        <button className="btn btn-sm btn-secondary ml-5">Home</button>
+      </div>
     </div>
   );
 };
--- src/Component/PremiumMember/PremiumMember.jsx
+++ src/Component/PremiumMember/PremiumMember.jsx
@@ -1,12 +1,26 @@
 import usePremiumMember from "../Hooks/usePremiumMember";
 import PremiumMemberCard from "./PremiumMemberCard";
 
+import Lottie from "lottie-react";
+import underSesign from "../../../src/Animation - 1717833435833.json";
+
 const PremiumMember = () => {
   const [premiumMember] = usePremiumMember();
   return (
     <div>
-      <h1>Premium Member: {premiumMember.length}</h1>
-      <h1 className="text-3xl text-center">Our Premium Members</h1>
+      {/* <h1>Premium Member: {premiumMember.length}</h1> */}
+
+      <div className="flex justify-center items-center">
+        <Lottie
+          className="mr-5 h-24 font-extrabold"
+          animationData={underSesign}
+        ></Lottie>
+        <h1 className="text-3xl text-center">Our Premium Members</h1>
+        <Lottie
+          className="ml-5 h-24 font-bold"
+          animationData={underSesign}
+        ></Lottie>
+      </div>
       <div className="grid grid-cols-3">
         {premiumMember.map((member) => (
           <PremiumMemberCard
